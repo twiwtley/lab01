7d23728de71dceb3e8d136e3d2dbfc2d34a5e1e6
twiwtley <37495492+twiwtley@users.noreply.github.com>
2018-04-15 15:17:17 -0700

    Add files via upload

--- block.cpp
+++ block.cpp
@@ -5,14 +5,18 @@ using namespace std;
 
 int main(){
         int rows, columns;
+	bool cont = true;
+	while (cont == true){
         cout << "Enter number of rows and columns:" << endl;
         cin >> rows >> columns;
-     for (int i=0; i<rows; i++){
-        for (int j=0; j<columns; j++){
-            cout << "X.";
-										                }
-               cout << endl;
-  }
- cout << endl;
-}
+	if (rows == 0 || columns == 0)
+		cont = false;
+	
+        for (int i=0; i<rows; i++){
+            for (int j=0; j<columns; j++){
+                cout << "X.";									                }
+            cout << endl;
+     }
 
+   }
+}
