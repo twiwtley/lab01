b23bb02074dc4cf0f12c04704bca12582aa873b2
twiwtley <37495492+twiwtley@users.noreply.github.com>
2018-04-15 15:39:30 -0700

    Update pi.cpp

--- pi.cpp
+++ pi.cpp
@@ -9,22 +9,23 @@ int main(){
         cout.setf(ios::showpoint);
         cout.precision(3);
 
-
         int n = 0, apr = 0;
-        
-	while (1){
-	double pi = 0;
+
+        while (1){
+        double pi = 0;
         cout << "Enter the value of the parameter 'n' in the Leibniz formula (or -1 to quit):" << endl;
         cin >> n;
         if (n == -1)
                 return 0;
         else{
-	        for (int i = 0; i < n; i++){
-	              pi += pow((-1.0), i)/(2*i+1);
-	        }
-	        pi = 4*pi;
+                n = n+1;
+                for (int i = 0; i < n; i++){
+                      pi += pow((-1.0), i)/(2*i+1);
+                }
+                pi = 4*pi;
         }
        cout << "The approximate value of pi using "<< n << " terms is: " << pi << endl;
-	}
+        }
+
 }
 
